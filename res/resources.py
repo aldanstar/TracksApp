bdf2274fb50af3c3edf36bfa8b8b53e8f634db6d
aldanstar <aldanstar@gmail.com>
2020-11-01 10:26:40 +0300

    update without models

---
+++ res/resources.py
@@ -0,0 +1,15 @@
+import os
+
+class resources:
+
+    GREEN_INDICATOR = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images','g.png'))
+    GREY_INDICATOR = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images', 'grey.png'))
+    RED_INDICATOR = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images', 'r.png'))
+    YELLOW_INDICATOR = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'images', 'y.png'))
+
+    SEPNN = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'separation_tracks.h5'))
+    SEGNN = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'semantic_tracks.h5'))
+    CNTNN = os.path.normpath(os.path.join(os.path.dirname(os.path.realpath(__file__)), 'counter_tracks.h5'))
+
+
+
